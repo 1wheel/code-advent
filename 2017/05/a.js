6b58b89414b408cea72dd3daa11c09f2c173806a
Adam Pearce <1wheel@gmail.com>
2017-12-05 00:19:36 -0500

    blah

---
+++ 2017/05/a.js
@@ -0,0 +1,30 @@
+var fs = require ('fs')
+var _ = require('underscore')
+var d3 = require('d3')
+var jp = require('d3-jetpack')
+
+var str = fs.readFileSync('b.txt', 'utf8').trim()
+var lines = str.split('\n').map(d => +d)
+
+
+console.log(lines)
+
+var i = 0
+var steps = 0
+
+
+while (0 <= i < lines.length){
+  var v = lines[i]
+  console.log(i, v, lines)
+
+  if (isNaN(v)) throw steps
+
+  lines[i]++
+  i += v
+
+  steps++
+}
+
+console.log(i, steps)
+
+function log(...args){ console.log.apply(null, args) }
